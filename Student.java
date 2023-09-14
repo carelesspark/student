63d0aba4ad9cf19771a690b2089ebcba6d4d5704
carelesspark <kiae1234@naver.com>
2023-09-14 18:01:03 +0900

    Student.java 수정

--- Student.java
+++ Student.java
@@ -1,15 +1,24 @@
-
+import lombok.Getter;
+import lombok.Setter;
 
 // lombok 적용
+@Getter
+@Setter
+
 public class Student {
 	private String name;
 	private String department;
 	private String id;
 	private double grade;
 	
-	//생성자 추가
-	
-	
-	
+	// 생성자 추가
+	public Student(String name, String department, String id, double grade) {
+		super();
+		this.name = name;
+		this.department = department;
+		this.id = id;
+		this.grade = grade;
+	}
+
 	// getter/setter 추가
 }
